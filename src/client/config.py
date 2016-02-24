7417c8e88030437b6a673763b41b8cad14ef700b
Rushi Agrawal <rushi.agr@gmail.com>
2016-02-24 16:05:39 +0530

    Add todo to config.py and remove outdated todo

--- src/client/config.py
+++ src/client/config.py
@@ -1,7 +1,6 @@
 # File to contain all the configuration values. Note that this is a python
 # file, so make sure you follow Python syntax throughout this file
 
-# TODO(rushiagr): add use_https config variable
 is_secure=False
 keystone_token_url='https://iam.ind-west-1.staging.jiocloudservices.com:5000/v3/auth/tokens'
 compute_url = 'http://10.140.214.69'
@@ -9,6 +8,9 @@ compute_url = 'https://compute.ind-west-1.staging.jiocloudservices.com/services/
 
 access_key = '67e4d6080c884d3382cc43aa5c52d963' # random dummy value
 secret_key = '71dc96bc558944b9824d30f4ca7aea42' # random dummy value
+
+# TODO(rushiagr): take username, account id and password as config values, and
+# write api call to generate access and secret for the same account
 
 # You can add same configs below so that you can override the above values with
 # new ones
