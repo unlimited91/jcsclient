6ebd7bb73795f3652a87aae14182561fccc7537a
Puneet Goyal <puneetgoyal728@gmail.com>
2016-03-29 13:19:14 +0530

    More comments incorporated

--- src/client/common.py
+++ src/client/common.py
@@ -52,46 +52,25 @@ def pkcs1_unpad(text):
             return text[pos+1:]
     return None
 
-def long_to_bytes (val, endianness='big'):
-    # From http://stackoverflow.com/questions/8730927/convert-python-long-int-to-fixed-size-byte-array
-
-    # one (1) hex digit per four (4) bits
+def long_to_bytes (val):
     try:
         #Python < 2.7 doesn't have bit_length =(
         width = val.bit_length()
     except:
         width = len(val.__hex__()[2:-1]) * 4
-
     # unhexlify wants an even multiple of eight (8) bits, but we don't
     # want more digits than we need (hence the ternary-ish 'or')
     width += 8 - ((width % 8) or 8)
-
-    # format width specifier: four (4) bits per hex digit
     fmt = '%%0%dx' % (width // 4)
-
-    # prepend zero (0) to the width, to zero-pad the output
     s = binascii.unhexlify(fmt % val)
-
-    if endianness == 'little':
-        # see http://stackoverflow.com/a/931095/309233
-        s = s[::-1]
-
     return s
 
 def decryptPassword(rsaKey, password):
-    #Undo the whatever-they-do to the ciphertext to get the integer
     encryptedData = base64.b64decode(base64.b64decode(password))
     ciphertext = int(binascii.hexlify(encryptedData), 16)
-    #print ("ciphertext = %d" % ciphertext)
-    #Decrypt it
     plaintext = rsaKey.decrypt(ciphertext)
-
-    #This is the annoying part.  long -> byte array
     decryptedData = long_to_bytes(plaintext)
-    #Now Unpad it
     unpaddedData = pkcs1_unpad(decryptedData)
-
-    #Done
     return unpaddedData
 
 
