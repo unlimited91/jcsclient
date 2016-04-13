07b277a72736c98d6a3ad929562c08b59bd54bac
hmdesai89 <hmdesai89@gmail.com>
2016-04-13 21:45:32 +0530

    rectifying typo

--- src/client/vpc_api/com.py
+++ src/client/vpc_api/com.py
@@ -220,7 +220,7 @@ def create_route(url, verb, headers, version, args):
     params['Version'] = version
     args = args[1:]
     parser = utils.get_argument_parser()
-    parser.add_argument('--router-table-id',required=True)
+    parser.add_argument('--route-table-id',required=True)
     parser.add_argument('--destination-cidr-block',required=True)
     parser.add_argument('--instance-id',required=True)
     args = parser.parse_args(args)
